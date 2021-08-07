fa54f626e9b3fa99ced7534fd8187d46837648a0
GinaZhan <59613993+GinaZhan@users.noreply.github.com>
2021-08-06 23:36:20 -0400

    DiceRoller image

--- MyAndroidApp/DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ MyAndroidApp/DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,45 +3,86 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+//    var diceImage : ImageView? = null
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-        val countUpButton: Button = findViewById(R.id.count_up)
-        countUpButton.setOnClickListener { countUp() }
-        val resetButton: Button = findViewById(R.id.reset)
-        resetButton.setOnClickListener { reset() }
+        val clearButton: Button = findViewById(R.id.clear_button)
+        clearButton.setOnClickListener { clearDice() }
+//        val countUpButton: Button = findViewById(R.id.count_up)
+//        countUpButton.setOnClickListener { countUp() }
+//        val resetButton: Button = findViewById(R.id.reset)
+//        resetButton.setOnClickListener { reset() }
     }
 
+    private fun clearDice() {
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource((R.drawable.empty_dice))
+    }
     private fun rollDice() {
-        val randomInt = (1..6).random()
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-//        resultText.text = "Dice Rolled!"
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
     }
 
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else if (resultText.text == "6") {
-            resultText.text = "6"
-        } else {
-            var originalInt = resultText.text.toString().toInt()
-            originalInt += 1
-            resultText.text = originalInt.toString()
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 
-    private fun reset() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0"
-    }
+//    private fun rollDice() {
+//        val randomInt = (1..6).random()
+////        Toast.makeText(this, "button clicked",
+////            Toast.LENGTH_SHORT).show()
+////        val resultText: TextView = findViewById(R.id.result_text)
+//////        resultText.text = "Dice Rolled!"
+////        resultText.text = randomInt.toString()
+//
+////        val diceImage: ImageView = findViewById(R.id.dice_image)
+//        val drawableResource = when (randomInt) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+//        diceImage.setImageResource(drawableResource)
+//    }
+
+//    private fun countUp() {
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        if (resultText.text == "Hello World!") {
+//            resultText.text = "1"
+//        } else if (resultText.text == "6") {
+//            resultText.text = "6"
+//        } else {
+//            var originalInt = resultText.text.toString().toInt()
+//            originalInt += 1
+//            resultText.text = originalInt.toString()
+//        }
+//    }
+//
+//    private fun reset() {
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        resultText.text = "0"
+//    }
 }
