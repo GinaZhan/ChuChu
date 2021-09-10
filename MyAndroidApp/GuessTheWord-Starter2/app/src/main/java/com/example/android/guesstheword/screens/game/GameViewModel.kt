74e05417a699ed3319a80e179ce4a27885a522ea
GinaZhan <59613993+GinaZhan@users.noreply.github.com>
2021-09-10 02:13:17 -0400

    Personal Project

--- MyAndroidApp/GuessTheWord-Starter2/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ MyAndroidApp/GuessTheWord-Starter2/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,21 +1,23 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    // The current word
-    // var word = ""
-    // var word = MutableLiveData<String>()
+    companion object {
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1000L
+        private const val COUNTDOWN_TIME = 60000L
+    }
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
 
-    // The current score
-    // var score = 0
-    // var score = MutableLiveData<Int>()
     private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
         get() = _score
@@ -23,6 +25,23 @@ class GameViewModel : ViewModel() {
     private val _eventGameFinish = MutableLiveData<Boolean>()
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime) {
+        time -> DateUtils.formatElapsedTime(time)
+    }
+
+    val wordHint = Transformations.map(word) { word ->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters" +
+                "\nThe letter at position " + randomPosition + " is " +
+                word.get(randomPosition - 1).toUpperCase()
+    }
+
+    private val timer: CountDownTimer
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -57,8 +76,19 @@ class GameViewModel : ViewModel() {
     init {
         _word.value = ""
         _score.value = 0
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
         resetList()
         nextWord()
+        timer.start()
 //        Log.i("GameViewModel", "GameViewModel created!")
     }
 
@@ -71,7 +101,8 @@ class GameViewModel : ViewModel() {
             //word = wordList.removeAt(0)
             _word.value = wordList.removeAt(0)
         } else {
-            onGameFinish()
+            // onGameFinish()
+            resetList()
         }
     }
 
@@ -90,6 +121,7 @@ class GameViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
+        timer.cancel()
     }
 
     /** Method for the game completed event **/
